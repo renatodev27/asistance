b2c72ea073c047294ad6b70c8a96568a3235f8fc
Renato Ramos <renatoramos.devops@gmail.com>
2021-09-29 19:18:26 -0500

    Added routes in routes and components

--- js/app.js
+++ js/app.js
@@ -1,10 +1,3 @@
-const home = Vue.component('home-view', {
-    template: '#principal',
-    data() {
-        return { }
-    }
-})
-
 const navbar = Vue.component('navbar-view', {
     template: '#navbar',
     data() {
@@ -24,17 +17,53 @@ const sidebar = Vue.component('sidebar-view', {
     template: '#sidebar',
 })
 
+/* HOME */
+const home = Vue.component('home-view', {
+    template: '#principal',
+    data() {
+        return { }
+    }
+})
+
+/* PROFILE */
+const profile = Vue.component('profile-view', {
+    template: '#profile'
+})
+
+/* NOTIFICATIONS */
+const notifies = Vue.component('notifies-view', {
+    template: '#notifies'
+})
+
+/* CHAT */
+const chat = Vue.component('chat-view', {
+    template: '#chat'
+})
+
+/* SETTINGS */
+const settings = Vue.component('settings-view', {
+    template: '#settings'
+})
+
+/* RECORDS */
+const records = Vue.component('records-view', {
+    template: '#records'
+})
+
+/* USERS */
 const users = Vue.component('users-view', {
     template: '#users',
     data() {
-        return {
-            
-        }
+        return { }
     }
 })
 
 const routes = [
     { path: '/home', component: home },
+    { path: '/notifications', component: notifies},
+    { path: '/chat', component: chat},
+    { path: '/settings', component: settings},
+    { path: '/records', component: records},
     { path: '/users', component: users},
 ]
 
